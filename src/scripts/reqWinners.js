d93fe5baf43c0df6aedab1b60d5ef5392fd07acf
tiago-py <tiago.braga0012@gmail.com>
2024-10-25 15:15:39 -0300

    [fix]corrigindo erros de caminho

--- src/scripts/reqWinners.js
+++ src/scripts/reqWinners.js
@@ -3,28 +3,28 @@ import { ref, onMounted } from 'vue';
 const winners = ref([]); // Renomeado para 'winners' para melhor compreensão
 const openIndex = ref(-1);
 
-const toggle = (index) => {
+export const toggle = (index) => {
     openIndex.value = openIndex.value === index ? -1 : index;
 };
 
-const isOpen = (index) => {
+export const isOpen = (index) => {
     return openIndex.value === index;
 };
 
 // Função para mascarar o CPF
-const maskCpf = (cpf) => {
+export const maskCpf = (cpf) => {
     if (!cpf) return 'CPF não disponível'; // Verifica se o CPF é definido
     return cpf.replace(/^(\d{3})\d{3}\d{3}(\d{2})$/, '$1.***.***-$2');
 };
 
 // Função para formatar o número da sorte
-const formatLuckyNumber = (luckyNumber) => {
+export const formatLuckyNumber = (luckyNumber) => {
     if (luckyNumber === undefined || luckyNumber === null) return 'Número da sorte não disponível'; // Verifica se o luckyNumber é válido
     return luckyNumber.toString().padStart(6, '0').replace(/(\d{3})(\d{3})/, '$1 - $2');
 };
 
 // Função para formatar a data
-const formatDate = (date) => {
+export const formatDate = (date) => {
     const parsedDate = Date.parse(date);
     if (isNaN(parsedDate)) {
         return 'Data inválida'; // Retorna mensagem padrão se a data não for válida
@@ -33,7 +33,7 @@ const formatDate = (date) => {
     return new Date(parsedDate).toLocaleDateString('pt-BR', options);
 };
 
-const fetchWinners = async () => {
+export const fetchWinners = async () => {
     try {
         const response = await fetch('https://landing-page-back-end.onrender.com/api/winners/');
         if (!response.ok) {
